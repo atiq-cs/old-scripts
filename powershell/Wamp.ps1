77dca268be9bc371fe4e1aeb8d10b6feb36e8d1f
Atiq Rahman <atiqcx@gmail.com>
2022-12-12 11:26:51 -0800

    AMP Stack Service related script from 2011 added

---
+++ powershell/Wamp.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+  AMP Stack Service Commands
+.DESCRIPTION
+  Date: 07/06/2011
+  Automate Services: httpd and mysql for now
+
+.EXAMPLE
+  Wamp start
+
+.NOTES
+  Prev name: IIS.ps1
+  Replace service cmdlets with Linux service commands for Linux.
+#>
+
+# TODO: use Param
+$cmd = [string] $args
+
+if ($cmd -eq "" -or $cmd.ToLower().Equals("start")) {
+    Write-Host -NoNewline "Starting service Apache`t"
+    Start-Service -DisplayName "World Wide Web Publishing Service"
+    echo "[OK]"
+    
+    <#Write-Host -NoNewline "Starting service MySQL`t"
+    Start-Service -DisplayName MySQL
+    echo "[OK]"#>
+}
+elseif ($cmd.ToLower().Equals("stop")) {
+    Stop-Service -DisplayName "World Wide Web Publishing Service"
+    #Stop-Service -DisplayName MySQL
+}
+elseif ($cmd.ToLower().Equals("restart")) {
+    # stop
+    Stop-Service -DisplayName Apache2.2
+    Stop-Service -DisplayName MySQL
+    # start
+    Start-Service -DisplayName Apache2.2
+    Start-Service -DisplayName MySQL
+}
+else {
+    echo "Please check command line arguments."
+}
